a1da11bac4b7a28f05b946fa22beecdd11e0d620
FelipeFreitas96 <felipe.samidare@hotmail.com>
2023-11-08 17:18:18 -0300

    feat(input): add input component and more style guide

--- src/app/icon/icon.module.ts
+++ src/app/icon/icon.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocumentComponent } from './document/document.component';
 import { RoundedIconDirective } from './icon.directive';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     DocumentComponent,
+    SearchComponent,
     RoundedIconDirective,
   ],
   exports: [
     DocumentComponent,
+    SearchComponent,
     RoundedIconDirective,
   ],
   imports: [
